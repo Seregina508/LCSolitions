3636d06c66c85d8e748ef09093022e75e8fa3739
Seregina508 <esgn508@gmail.com>
2023-03-01 18:20:42 -0500

    nested loops

--- Patterns.js
+++ Patterns.js
@@ -111,6 +111,24 @@
 // }
 // console.log(pattern(7));
 
+// function pattern(n){
+//     let str = '';
+//     for (let i = 1; i <= n; i+= 2){
+//         let space = '';
+//         let stars = '';
+//         for (let j = 1; j <= (n - i)/2; j++){
+//             space = space + '_';
+//         }
+//         for (let j = 1; j <= i; j++){
+//             stars = stars + '*';
+//         }
+//         str = str + space + stars + space;
+//         str = (i === n) ? str : str + '\n';
+//     }
+//     return str;
+// }
+// console.log(pattern(7));
+
 // #
 // _#
 // __#
